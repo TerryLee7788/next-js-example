22b44fa6c91364f8133c31bc8d43bbce42689a49
TerryLee <k9114658@gmail.com>
2019-06-07 22:12:17 +0800

    Add for Loading

--- pages/User.js
+++ pages/User.js
@@ -1,8 +1,108 @@
 import React from 'react';
 import withWrap from '../hoc/withWrap';
+import Router from 'next/router'
 
-function User (props) {
-    return <div>hi: {props.name}</div>
+class User extends React.Component {
+
+    constructor (props) {
+        super(props);
+        this.state = {
+            showLoading: false
+        };
+
+    }
+
+    static async getInitialProps ({ req, ctx }) {
+
+        let data = {};
+
+        console.log('csr');
+
+        await new Promise((resolve) => {
+            setTimeout(() => {
+
+                resolve();
+                data.gggg = 'ggg';
+                console.log('ggyy: ', this);
+
+            }, 5000)
+        })
+        // if (Component.getInitialProps) {
+        //     pageProps = await Component.getInitialProps(ctx)
+        // }
+
+        return { data, json, pageProps };
+    }
+
+    showLog = () => {
+
+        console.log(1111);
+
+    }
+
+    stopLoading = async () => {
+
+        console.log('stopLoading');
+        await new Promise((res) => {
+
+            setTimeout(() => {
+
+                res();
+
+                console.clear();
+
+            }, 3000);
+
+        })
+
+        this.setState({
+            showLoading: false
+        });
+
+    }
+
+    showLoading = () => {
+
+        console.log('showLoading');
+
+        this.setState({
+            showLoading: true
+        });
+
+    }
+
+    componentWillUnmount () {
+
+        console.log('componentWillUnmount =====');
+        Router.events.off('routeChangeStart', this.showLoading);
+        Router.events.off('routeChangeComplete', this.stopLoading);
+
+    }
+
+    componentDidMount () {
+
+        console.log('===== componentDidMount');
+        Router.events.on('routeChangeStart', this.showLoading);
+        Router.events.on('routeChangeComplete', this.stopLoading);
+
+    }
+
+    render () {
+
+        console.log('this.props: ', this.props);
+
+        return (
+            <div>
+                hi: {this.props.name}
+                {
+                    this.state.showLoading
+                        ? (<div>loading...</div>)
+                        : (null)
+                }
+            </div>
+        );
+    }
+
 }
-  
+
 export default withWrap(User);
