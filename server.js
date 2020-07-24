ab3577a75d45ae25d4e2a6a498346ab444e861d2
TerryLee <Terry.lee@ehsn.com.tw>
2020-07-24 17:25:06 +0800

    update server.js PORT

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const PORT = process.env.PORT || 3000
 
 /*
 app.prepare().then(() => {
@@ -65,9 +66,9 @@ app
             return handle(req, res);
         });
 
-        server.listen(3000, err => {
+        server.listen(PORT, err => {
             if (err) throw err;
-            console.log('> Ready on http://localhost:3000');
+            console.log(`> Ready on http://localhost:${PORT}`);
         });
 
     })
